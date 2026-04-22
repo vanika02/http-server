18e54ffcfcb262977c01b31cdb8e2c3f10a342b8
vanika02 <vanikadangi02@gmail.com>
2026-04-22 10:53:00 +0000

    updated server.py to reflect changes in the rest 2 files

--- server.py
+++ server.py
@@ -1,13 +1,12 @@
 import socket
 import json
+from router import route
 
 HOST = '127.0.0.1'
 PORT = 8080
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 server_socket.bind((HOST, PORT))
-
 server_socket.listen(1)
 
 print(f"server running on http://{HOST}:{PORT}")
@@ -28,29 +27,7 @@ while True:
     request_line = request.split('\n')[0]
     method, path, version = request_line.split()
 
-    # routing
-    if path == "/":
-        body = "Hello vanika"
-        content_type = "text/plain"
-        status = "200 ok"
-
-    elif path == "/about":
-        body = "About page"
-        content_type = "text/plain"
-        status = "200 ok"
-
-    elif path == "/api":
-        body = json.dumps({
-            "name": "Vanika",
-            "role": "Backroom anhilator"
-        })
-        content_type= "application/json"
-        status = "200 ok"
-    
-    else:
-        body = "404 not found"
-        content_type = "text/plain"
-        status = "404 Not Found"
+    status, content_type, body = route(path)
 
     # print("Method: ", method)
     # print("Path: ", path)
@@ -62,5 +39,4 @@ while True:
     )
 
     client_socket.send(response.encode())
-
     client_socket.close()
