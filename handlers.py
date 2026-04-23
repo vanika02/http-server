1b2ec8b53abb04e69377da93769e7bb37cb4ec6b
vanika02 <vanikadangi02@gmail.com>
2026-04-23 15:37:45 +0000

    added signup func in handlers

--- handlers.py
+++ handlers.py
@@ -1,5 +1,7 @@
 import json
 
+users = {}
+
 def home():
     return "200 OK", "text/plain", "Hello Dharampal"
 
@@ -16,6 +18,27 @@ def api():
 def login(body):
     return "200 OK", "application/json", f'{{"message":"Welcome {body}"}}'
 
+def signup(body):
+    data = json.loads(body)
 
+    username = data.get("username")
+    password = data.get("password")
+
+    if not username or password:
+        return "400 Bad Request", "application/json", json.dumps({
+            "message": "Missing username or password"
+        })
+
+    if username in users:
+        return "409 confilct", "application/json", json.dumps({
+            "message": "User already exists"
+        })
+    
+    users[username] = password
+
+    return "201 created", "application/json", json.dumps({
+        "message": "User created"
+    })
+    
 def not_found():
     return "404 You can't see me", "text/plain", "404 Not Found"
