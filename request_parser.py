8f28c2e3d22e0434104d23240d1d92d63e82ee16
vanika02 <vanikadangi02@gmail.com>
2026-07-11 06:13:10 +0000

    example http parser commented

--- request_parser.py
+++ request_parser.py
@@ -1,14 +1,16 @@
 from http_parse import parse
 
-raw_request = """
-GET /path HTTP/1.1
-Host: example.com
-User-Agent: curl/8.5.0
+# raw_request = """
+# GET /path HTTP/1.1
+# Host: example.com
+# User-Agent: curl/8.5.0
 
-{"data": "example"}
-"""
+# {"data": "example"}
+# """
 
-parsed = parse(raw_request)
-print("Headers:", parsed.headers)       # Access headers (dictionary format)
-print("Body:", parsed.body)          # Access Body
-print("Path:", parsed.path)          # Access requested path (/foo/bar)
+# parsed = parse(raw_request)
+# print("Headers: ", parsed.headers)       # Access headers (dictionary format)
+# print("Body: ", parsed.body)          # Access Body
+# print("Path: ", parsed.path)          # Access requested path (/foo/bar)
+# print("Method: ", parsed.method)
+
