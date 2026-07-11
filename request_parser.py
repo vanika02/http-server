89546c70befb3315a32b0ddf1832ec31aea24169
vanika02 <vanikadangi02@gmail.com>
2026-07-11 06:24:34 +0000

    HTTPRequest class created to parse http request

--- request_parser.py
+++ request_parser.py
@@ -1,5 +1,29 @@
 from http_parse import parse
 
+class HTTPRequest:
+    """
+    A class representing an HTTP request.
+
+    Attributes:
+        headers (dict): Dictionary of HTTP headers.
+        body (str): Request body.
+        method (str): HTTP method (GET, POST, etc.)
+        path (str): Requested path
+        http_version (str): HTTP version
+    """
+    
+
+
+
+
+
+
+
+
+
+
+
+
 # raw_request = """
 # GET /path HTTP/1.1
 # Host: example.com
@@ -13,4 +37,3 @@ from http_parse import parse
 # print("Body: ", parsed.body)          # Access Body
 # print("Path: ", parsed.path)          # Access requested path (/foo/bar)
 # print("Method: ", parsed.method)
-
