d36454e370b3a2e8221f8db0b12a8465c3472097
vanika02 <vanikadangi02@gmail.com>
2026-07-17 09:59:11 +0000

    more status code added

--- http_response.py
+++ http_response.py
@@ -3,10 +3,12 @@ class HTTPResponse:
     STATUS_CODES = {
         200: "OK",
         201: "Created",
+        204: "No Content",
         400: "Bad Request",
         401: "Unauthorized",
         403: "Forbidden",
         404: "Not Found",
+        405: "Method not allowed",
         500: "Internal Server Error"
     }
 
